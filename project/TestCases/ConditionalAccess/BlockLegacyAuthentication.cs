36e6efb14e8fc9fb9763741f4f8556f77b496b63
Jing-Jenny-Li <119757942+Jing-Jenny-Li@users.noreply.github.com>
2022-12-03 14:17:09 -0800

    Update BlockLegacyAuthentication.cs

--- project/TestCases/ConditionalAccess/BlockLegacyAuthentication.cs
+++ project/TestCases/ConditionalAccess/BlockLegacyAuthentication.cs
@@ -29,8 +29,10 @@ namespace TestCases.ConditionalAccess
             var result = false;
             foreach (var policy in policies)
             {
-
-                if(policy.DisplayName == "Block legacy authentication")
+                /*Jenny-2022-policy of blocking legacy authentication
+                  for future work, change fix string to variable input that can be configured by user
+                */
+                if(policy.DisplayName == "Block legacy authentication")//the name should be configured instead of hard code
                 {
                     result = policy.State == ConditionalAccessPolicyState.Enabled;
                 }
